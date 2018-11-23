173d067edf324f7fb924d1f0d92c4919bb495a0c
Jon Kimbel <helpimtrappedinanemailaddress@gmail.com>
2018-11-22 17:59:00 -0800

    Update tests

--- appengine/src/test/java/com/jonkimbel/busboybackend/BusBoyServletTest.java
+++ appengine/src/test/java/com/jonkimbel/busboybackend/BusBoyServletTest.java
@@ -76,7 +76,7 @@ public class BusBoyServletTest {
     // We expect our hello world response.
     assertThat(responseWriter.toString())
         .named("BusBoyServlet response")
-        .contains("Hello App Engine - Standard ");
+        .isEqualTo("Hello busboy");
   }
 }
 // [END example]
